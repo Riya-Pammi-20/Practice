0dfab10a4272dbb5c8ed8553a1e24c8301f81c75
Riya-Pammi-20 <riyahello20@gmail.com>
2024-10-01 19:07:11 +0530

    Update vars.tf

--- forEach_Count/vars.tf
+++ forEach_Count/vars.tf
@@ -1,18 +1,20 @@
-variable "REGION"{
-    type = list(string)
-    default = ["us-east-1", "us-east-2"]
+variable "REGION" {
+  type    = list(string)
+  default = ["us-east-1", "us-east-2"]
 }
 
-variable "AMIs"{
-    type = map(string)
-    region = var.REGION
-    default ={
-        "us-east-1" = "ami-0ebfd941bbafe70c6"
-        "us-east-2" = "ami-037774efca2da0726"
-    }
+variable "AMIs" {
+  type = map(object({
+    ami = string
+    az  = string
+  }))
+  default = {
+    "us-east-1" = { ami = "ami-0ebfd941bbafe70c6", az = "us-east-1a" }
+    "us-east-2" = { ami = "ami-0aa8fc2422063977", az = "us-east-2b" }
+  }
 }
 
-variables "availability_zones"{
-    type = list(string)
-    default = ["us-east-1a","us-east-2b"]  
+variable "availability_zone" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-2b"]
 }
