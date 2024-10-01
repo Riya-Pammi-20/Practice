830cd67604a28e221de2ff79518a15e415d0f8ed
Riya-Pammi-20 <riyahello20@gmail.com>
2024-10-01 18:05:00 +0530

    Update vars.tf

--- RemotestateAWS/remoteStateAWS/vars.tf
+++ RemotestateAWS/remoteStateAWS/vars.tf
@@ -1,31 +1,27 @@
-variable "REGION"{
-    type = list(string)
-    default = ["eu-west-1", "us-east-1", "us-east-2"]
+variable "REGION" {
+  default = "eu-west-1"
 }
 
-variable "availability_zones"{
-    type = list(string)
-    default = [ "eu-west-1a", "eu-west-1b", "us-east-1a", "us-east-1b","us-east-2a" ,"us-east-2b" ]
+variable "availability_zones" {
+  default = "eu-west-1a"
 }
 
 variable "AMIs" {
-    type = map(string)
-    default = {
-        eu-west-1 = "ami-0fed63ea358539e44"
-        us-east-1 = "ami-0ebfd941bbafe70c6"
-        us-east-2 = "ami-037774efca2da0726"
-    }
- 
-  
+  type = map(string)
+  default = {
+    eu-west-1 = "ami-0fed63ea358539e44"
+    us-east-1 = "ami-0ebfd941bbafe70c6"
+    us-east-2 = "ami-037774efca2da0726"
+  }
+
+
 }
 
-variable "tags"{
-    type = map(string)
-    default ={
-        Name    = "demo2"
-        Project = "terra-handson"
-    }
-   
+variable "tags" {
+  type = map(string)
+  default = {
+    Name    = "demo2"
+    Project = "terra-handson"
+  }
+
 }
-
-
