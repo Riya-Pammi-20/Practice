56a5006a25f3e83243bb8a50853a2406cf5af651
Riya-Pammi-20 <riyahello20@gmail.com>
2024-10-01 18:03:52 +0530

    Update instance.tf

--- RemotestateAWS/remoteStateAWS/instance.tf
+++ RemotestateAWS/remoteStateAWS/instance.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "demo-instance" {
-  ami                    = var.AMIs
+  ami                    = var.AMIs[var.REGION]
   instance_type          = "t2.micro"
   key_name               = "vpro-cf"
   vpc_security_group_ids = ["sg-0ddd95bca4bdd9533"]
-  tags = var.tags
-  availability_zones = var.availability_zones
+  tags                   = var.tags
+  availability_zone      = var.availability_zones
 
 }
