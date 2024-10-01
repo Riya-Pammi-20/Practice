085a51128e8d365b3476f2478ddeb773ab84b35c
Riya-Pammi-20 <riyahello20@gmail.com>
2024-10-01 19:06:06 +0530

    Update instance.tf

--- forEach_Count/instance.tf
+++ forEach_Count/instance.tf
@@ -1,8 +1,6 @@
-resource "aws_instance" "demo3"{
-    for_each = toset(var.REGION)
-    region = each.key
-    type = "t2.micro"
-    ami = var.AMIs[each.key]
-    availability_zones = var.availability_zones
+resource "aws_instance" "demo3" {
+  for_each          = var.AMIs
+  instance_type     = "t2.micro"
+  ami               = each.value.ami
+  availability_zone = each.value.az
 }
-
