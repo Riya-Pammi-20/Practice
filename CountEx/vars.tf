6d508ba620ae0917b64262c6cfc0133dfb77cb05
Riya-Pammi <riyapam20@gmail.com>
2024-10-02 12:38:48 +0000

    correct code

--- CountEx/vars.tf
+++ CountEx/vars.tf
@@ -1,50 +1,38 @@
-variable "REGION"{
-    default = "eu-west-1"
+variable "REGION" {
+  default = "ap-south-1"
 }
 
-variable "av_zones"{
-    type = list(string)
-    default = ["eu-west-1a" , "eu-west-1b"]
+variable "av_zones" {
+  default = "ap-south-1a"
 }
 
-variable "tags"{
-    type = map(string)
-    default = {
-        "Name" = "Inst"
-    }
+
+variable "AMIs" {
+  type = map(object({
+    tag = string
+    ami = string
+
+  }))
+  default = {
+    "AmazonLinux" = { ami = "ami-08718895af4dfa033", tag = "AmazonLinux" }
+    "Ubuntu"      = { ami = "ami-0dee22c13ea7a9a67", tag = "Ubuntu" }
+    "RedHat"      = { ami = "ami-022ce6f32988af5fa", tag = "RedHat" }
+
+  }
+
 }
 
-variable "AMIs"{
-    type = list(string)
-    default = ["ami-08718895af4dfa033","",""]
-}
+locals {
+  ami_list = [
+    var.AMIs["AmazonLinux"].ami,
+    var.AMIs["Ubuntu"].ami,
+    var.AMIs["RedHat"].ami
+  ]
 
-variable "AMIs"{
-    type = map(object({
-        tag =  String
-        ami = String
+  tag_list = [
+    var.AMIs["AmazonLinux"].tag,
+    var.AMIs["Ubuntu"].tag,
+    var.AMIs["RedHat"].tag
 
-    }))
-    default = {
-    "AmazonLinux" = { ami ="ami-08718895af4dfa033", tag = "AmazonLinux"}
-    "Ubuntu" = { ami = "ami-0dee22c13ea7a9a67", tag = "Ubuntu"}
-    "RedHat" = { ami = "ami-022ce6f32988af5fa", tag = "RedHat"}
-
-    }
- 
-}
-
-locals{
-    ami_list[ 
-        var.AMIs["AmazonLinux"].ami, 
-        var.AMIs["Ubuntu"].ami,
-        var.AMIs["RedHat"].ami 
-        ]
-
-    tag_list[
-        var.AMIs["AmazonLinux"].tag,
-         var.AMIs["Ubuntu"].tag,
-        var.AMIs["RedHat"].tag
-        
-        ]
+  ]
 }
